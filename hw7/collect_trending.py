add7e158c6da20624c6774a7ad907f070f8c83b2
Jules Lanari-Collard <jules_collard@yahoo.co.uk>
2024-10-21 17:23:06 +0000

    hw7 partial solutions

---
+++ hw7/collect_trending.py
@@ -0,0 +1,38 @@
+import argparse, bs4, requests
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="TrendingCollector",
+        description="Collects trending articles from montrealgazette.com/category/news/"
+    )
+    parser.add_argument("-o", "--output", help="Output JSON file", required=True)
+    parser.parse_args()
+
+
+def get_trending(download=False, filename="frontpage.html"):
+    
+    # Download front page HTML content
+    if download:
+        driver = webdriver.Chrome(ChromeDriverManager().install())
+        driver.maximize_window()
+        driver.get("https://montrealgazette.com/category/news/")
+        
+        with open(filename, "w") as f:
+            f.write(driver.page_source)
+        
+        driver.quit()
+
+    with open(filename, "r") as f:
+        soup = bs4.BeautifulSoup(f, "html.parser")
+    
+    headlines = soup.find_all("h3", attrs={"class":"article-card_headline"})
+    for h in headlines:
+        print(h.text)
+
+
+if __name__ == "__main__":
+    get_trending(download=True)
+    
+
---
+++ hw7/conceptual_answers.md
@@ -0,0 +1,3 @@
+1. Found data are data produced for other purposes, where as designed data arise from surveys or experiments designed specifically for the project.
+2. Sampling can be needed when it is difficult/expensive/impossible to collect data on the whole population.
+3. If an API is not provided, website owners have not given explicit permission for their website data to be used for other purposes.
